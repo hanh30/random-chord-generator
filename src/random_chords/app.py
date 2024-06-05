820fef61c22409756c0036c90e403ec31fba71a7
Le Hanh <hanh3h3@gmail.com>
2024-06-06 00:02:00 +0700

    fix

--- src/random_chords/app.py
+++ src/random_chords/app.py
@@ -6,9 +6,11 @@ import subprocess
 
 import os
 os.system('Xvfb :1 -screen 0 1600x1200x16  &')    # create virtual display with size 1600x1200 and 16 bit color. Color can be changed to 24 or 8
-os.environ['DISPLAY']=':1.0' 
+os.environ['DISPLAY']=':1.0'  # tell X clients to use our virtual DISPLAY :1.0
 
 environment.set('musescoreDirectPNGPath', '/usr/bin/mscore')
+
+subprocess.run(["export", "QT_QPA_PLATFORM=offscreen"])
 
 # subprocess.run(["chmod", "+x", "/mount/src/random-chord-generator/src/random_chords/musescore_setup.sh"])
 # subprocess.run(["/mount/src/random-chord-generator/src/random_chords/musescore_setup.sh"], shell=True)
